a9b71a4be6c7450f46e0943a38d38f192b78228b
Brian Di Palma <offler@gmail.com>
2015-10-09 20:51:50 +0100

    feat: gather metadata for more than one video

--- request_videos.js
+++ request_videos.js
@@ -1,50 +1,90 @@
 #!/usr/bin/env node
+/*eslint-disable no-console*/
 "use strict";
 
-/**
- * Script to download videos from websites.
- */
+// Script to download videos from websites.
 
 const fs = require("fs");
 
 const got = require("got");
 const cheerio = require("cheerio");
+const minimist = require("minimist");
 
-// Given a `videoURL` download it to the pwd.
-function downloadVideo(videoURL) {
-	// The last part of the URL ("dir/videos/the_video.mp4") should contain the name of video file.
-	const videoFileName = videoURL.split("/").pop();
+// Accepts the CLI argument `n` which can be used to configure the max number of videos to download.
+const args = minimist(process.argv.slice(2), {
+	default: {n: 30}
+});
 
-	got
-		.stream(videoURL)
-		.pipe(fs.createWriteStream(videoFileName));
-}
+// Given a `videoURL` download it to the cwd.
+// function downloadVideo(videoURL, videoFileName) {
+// 	got
+// 		.stream(videoURL)
+// 		.pipe(fs.createWriteStream(videoFileName));
+// }
 
-// Called back with response that contains HTML from FT's video website.
-function extractFTVideoMetaData(response) {
+// Called back with response that contains HTML from FT's video website. Will extract the webpage's video URL
+// and the next video's website URL.
+function extractFTVideoMetadata(response) {
 	const $ = cheerio.load(response.body);
 
 	const nextVideoHTMLURL = $(".coming-next .video-thumb-link").attr("href");
 	const currentVideoURL = $("meta[property='twitter:player:stream']").attr("content");
+	// The last part of the URL ("dir/videos/the_video.mp4") should contain the name of video file.
+	const videoFileName = currentVideoURL.split("/").pop();
 
-	return {currentVideoURL, nextVideoHTMLURL};
+	return {currentVideoURL, nextVideoHTMLURL, videoFileName};
 }
 
-function getFTVideoURLs() {
+// Check if a file exists.
+function doesFileExist(fileName) {
+	try {
+		fs.statSync(fileName);
+
+		// If no error was raised the file exists.
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
+
+//
+function requestNextVideoURL(videosMetadata, resolve, reject) {
+	return (videoPageMetadata) => {
+		const videoFileExists = doesFileExist(videoPageMetadata.videoFileName);
+
+		if (videoFileExists) {
+			console.log(`Video ${videoPageMetadata.videoFileName} already exists.`);
+
+			resolve(videosMetadata);
+		} else {
+			videosMetadata.push(videoPageMetadata);
+
+			if (videosMetadata.length < args.n) {
+				getFTVideosMetadata(videoPageMetadata.nextVideoHTMLURL, videosMetadata)
+					.then(resolve)
+					.catch(reject);
+			} else {
+				resolve(videosMetadata);
+			}
+		}
+	};
+}
+
+// Will push into the `videosMetadata` array video website metadata until the maximum number of videos to download
+// limit has been reached.
+function getFTVideosMetadata(videoHTMLURL, videosMetadata) {
 	return new Promise((resolve, reject) => {
-		return got("http://video.ft.com/latest")
-			.then(extractFTVideoMetaData)
-			.then(resolve)
+		got(videoHTMLURL)
+			.then(extractFTVideoMetadata)
+			.then(requestNextVideoURL(videosMetadata, resolve, reject))
 			.catch(reject);
 	});
 }
 
-getFTVideoURLs()
-	/*eslint-disable no-console*/
+getFTVideosMetadata("http://video.ft.com/latest", [])
 	.then(metadata => {
 		console.log(metadata);
 
-		downloadVideo(metadata.currentVideoURL);
+		// downloadVideo(metadata.currentVideoURL, metadata.videoFileName);
 	})
 	.catch(console.error);
-	/*eslint-enable no-console*/
